f844185ca6fe552c7d1116d3e4747caaae1c4a42
worawut33824-alt <worawut33824@gmail.com>
2026-06-03 20:20:07 +0700

    remove blessings.html - moved to dashboard tab

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Wedding Planner – Service Worker
 // Caches core assets for offline use
 
-const CACHE = 'wedding-planner-v72';
+const CACHE = 'wedding-planner-v73';
 const ASSETS = [
   './',
   './index.html',
