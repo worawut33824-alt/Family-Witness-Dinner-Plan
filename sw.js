cbe08ee46af22e02c5d9ff3935caaea7cfb44684
worawut33824-alt <worawut33824@gmail.com>
2026-06-03 20:59:14 +0700

    fix: blessing card title to ไม่สะดวกมาร่วมงาน

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Wedding Planner – Service Worker
 // Caches core assets for offline use
 
-const CACHE = 'wedding-planner-v80';
+const CACHE = 'wedding-planner-v81';
 const ASSETS = [
   './',
   './index.html',
