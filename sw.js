bd94950b25c456384f1afc7ebde1f957928b2349
worawut33824-alt <worawut33824@gmail.com>
2026-06-03 20:05:35 +0700

    update SCRIPT_URL to latest GAS deployment

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Wedding Planner – Service Worker
 // Caches core assets for offline use
 
-const CACHE = 'wedding-planner-v70';
+const CACHE = 'wedding-planner-v71';
 const ASSETS = [
   './',
   './index.html',
