10546b83d1bf25f2ea73de8bfea42adc871fa3bd
worawut33824-alt <worawut33824@gmail.com>
2026-06-03 09:56:33 +0700

    feat: add Our Story narrative text before video in sec-story

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Wedding Planner – Service Worker
 // Caches core assets for offline use
 
-const CACHE = 'wedding-planner-v63';
+const CACHE = 'wedding-planner-v64';
 const ASSETS = [
   './',
   './index.html',
