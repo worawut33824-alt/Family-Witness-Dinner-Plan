4fcf6f67a5e9b70112bab4d77a9e73ac8c8c8ec0
worawut33824-alt <worawut33824@gmail.com>
2026-06-03 20:26:38 +0700

    set default SCRIPT_URL in dashboard

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Wedding Planner – Service Worker
 // Caches core assets for offline use
 
-const CACHE = 'wedding-planner-v74';
+const CACHE = 'wedding-planner-v75';
 const ASSETS = [
   './',
   './index.html',
